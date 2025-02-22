fdda16692f50b86c83d4d4a70dadf9e699ab5c94
BenjaminPring <73898228+BenjaminPring@users.noreply.github.com>
2025-02-22 11:33:00 +0100

    TailwindCSS and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="text-3xl text-white-700 font-black">
-      <h1>Welcome to the world of Next.js</h1>
-    </div>
+
+      <h1 className= "h1-bold">Welcome to the world of Next.js</h1>
+    
   );
 }
 
